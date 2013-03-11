d35ff0a2cbea43219565e19138ea735e59f6ddf4
Cristina <cristina@LinuxPC.(none)>
2013-03-11 16:42:39 +0200

    Create app v 2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alumni::Application.config.secret_token = '402ffcb2017a689af9e5e59917aae107474f7797ef821c5614e42e3fc8f4fbe725da682fb7f59ac5a22a55a554517ea8dfa5945d9403c914c7e88f6d75fa0111'
