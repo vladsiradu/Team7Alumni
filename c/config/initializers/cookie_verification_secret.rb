cbea3fc5df5058b4825c179cb46aad08d9e6a70c
Alexandra <alex@ubuntu.ubuntu-domain>
2013-03-12 14:37:02 +0200

    Sterge chestii

--- c/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'c1d13ce620f08cd545fbe708adac509134bc4e401be0ce246a0e5b33db71ba2bc7c46d0db28fe2e4f1a85ae79006a102d4008bf7c29341e224c04deb38d552c6';
