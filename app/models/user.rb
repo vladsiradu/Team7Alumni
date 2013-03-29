f2c7d8861e7cc7442a43e1e371fafa7763d89c04
Cristina <cristina@LinuxPC.(none)>
2013-03-29 15:51:09 +0200

    Deleted some addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,8 @@ geocoded_by :location
 # auto-fetch coordinates
 after_validation :geocode
 
-reverse_geocoded_by :latitude, :longitude do |obj,results|
-  if geo = results.first
-    obj.city    = geo.city
-    obj.zipcode = geo.postal_code
-    obj.country = geo.country_code
-  end
-end
+reverse_geocoded_by :latitude, :longitude
+
 # auto-fetch address
 after_validation :reverse_geocode  
 
